32895f2fe2e3a9157ab25eef7c048cfa7c72c8d3
Juan A. Cortes <jcortes0309@gmail.com>
2012-08-29 21:18:50 -0400

    Updated to include HAML, Sorcery and Paperclip

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,18 @@
 class TagsController < ApplicationController
   def index
+  	@tags = Tag.all
   end
 
   def show
+  	@tag = Tag.find(params[:id])
+  end
+
+  def destroy
+  	@tag = Tag.find(params[:id])
+  	@tag.destroy
+
+    flash[:message] = "Tag '#{@tag.name}' deleted"
+
+  	redirect_to articles_path(@articles)
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,7 @@
 class Tag < ActiveRecord::Base
   attr_accessible :name
 
-  has_many :taggings
+  has_many :taggings, :dependent => :destroy
   has_many :articles, :through => :taggings
 
   def to_s
